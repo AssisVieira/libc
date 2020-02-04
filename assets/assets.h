cb34400b52e27594512909a5cad0c5cfd1cbab25
Assis Vieira <assis.sv@gmail.com>
2020-02-04 19:55:12 -0300

    Comments for the module assets.

--- assets/assets.h
+++ assets/assets.h
@@ -7,21 +7,31 @@
  * Carrega para a memória todos os arquivos do diretório e subdiretórios
  * de um endereço.
  *
- * @param  path diretório raiz dos assets.
+ * @param  path endereço relativo do diretório raiz.
  * @return      0, em caso de sucesso, -1 em caso de erro.
  */
 int assets_open(const char *path);
 
 /**
- * Obtém o cursor de leitura do
+ * Obtém o conteúdo de um arquivo.
  *
- * @param  path [description]
- * @return      [description]
+ * @param  path endereço relativo do arquivo.
+ * @return      conteúdo do arquivo.
  */
 const char *assets_get(const char *path);
 
+/**
+ * Obtém o tamanho de um arquivo.
+ *
+ * @param path endereço relativo do arquivo.
+ * @return     quantidade de bytes do arquivo.
+ */
 size_t assets_size(const char *path);
 
+/**
+ * Fecha o módulo assets, liberando toda a memória utilizada pelo módulo,
+ * incluive o conteúdo dos arquivos carregados.
+ */
 void assets_close();
 
 #endif
