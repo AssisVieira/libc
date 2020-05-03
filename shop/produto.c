4c93ed52d111bc75079673c7d205140fde5f8a1e
Assis Vieira <assis.sv@gmail.com>
2020-05-03 01:14:51 -0300

    [shop] Refactoring produto.c

--- shop/produto.c
+++ shop/produto.c
@@ -10,7 +10,7 @@ void produto_init(DBPool *pool) {
 
 Produto *produto_new(const char *nome) { 
   Produto *produto = malloc(sizeof(Produto));
-  strncpy(produto->nome, nome, sizeof(produto->nome));
+  strncpy(produto->nome, nome, PROD_NOME_SIZE);
   return produto;
 }
 
@@ -19,8 +19,6 @@ void produto_free(Produto *produto) {
 }
 
 int produto_add(const Produto *produto) {
-  int r = 0;
-
   DB* db = db_pool_get(gPool);
 
   db_sql(db, 
@@ -35,13 +33,9 @@ int produto_add(const Produto *produto) {
   db_paramInt(db, produto->parcelas);
   db_param(db, produto->descricao);
 
-  if (db_exec(db)) {
-    r = -1;
-  }
+  db_exec(db);
 
-  db_close(db);
-
-  return r;
+  return db_close(db);
 }
 
 int produto_buscar(Produto *produtos, int pg, int pgSize) {
@@ -54,27 +48,21 @@ int produto_buscar(Produto *produtos, int pg, int pgSize) {
   db_paramInt(db, pg);
   db_paramInt(db, pgSize);
 
-  if (db_exec(db)) {
-    count = -1;
-  } else {
-    for (int i = 0; i < db_count(db); i++) {
-      strncpy(produtos[i].codigo, db_value(db, i, 0), sizeof(produtos[i].codigo));
-      strncpy(produtos[i].nome, db_value(db, i, 1), sizeof(produtos[i].nome));
-      produtos[i].precoTabela = db_valueDouble(db, i, 2);
-      produtos[i].preco = db_valueDouble(db, i, 3);
-      produtos[i].parcelas = db_valueInt(db, i, 4);
-      strncpy(produtos[i].descricao, db_value(db, i, 5), sizeof(produtos[i].descricao));
-
-      count++;     
-
-      if (db_error(db)) {
-        count = -1;
-        break;
-      }
-    }
+  db_exec(db);
+  
+  for (int i = 0; i < db_count(db); i++) {
+    strncpy(produtos[i].codigo, db_value(db, i, 0), PROD_CODI_SIZE);
+    strncpy(produtos[i].nome, db_value(db, i, 1), PROD_NOME_SIZE);
+    produtos[i].precoTabela = db_valueDouble(db, i, 2);
+    produtos[i].preco = db_valueDouble(db, i, 3);
+    produtos[i].parcelas = db_valueInt(db, i, 4);
+    strncpy(produtos[i].descricao, db_value(db, i, 5), PROD_DESC_SIZE);
+    count++;     
   }
 
-  db_close(db);  
+  if (db_close(db)) {
+    return -1;
+  }
 
   return count;
 }
--- shop/produto.h
+++ shop/produto.h
@@ -3,21 +3,36 @@
 
 #include "db/db.h"
 
+#define PROD_CODI_SIZE 128
+#define PROD_NOME_SIZE 128
+#define PROD_DESC_SIZE 4096
+
 typedef struct Produto {
-  char codigo[128];
-  char nome[128];
+  char codigo[PROD_CODI_SIZE];
+  char nome[PROD_NOME_SIZE];
   double precoTabela;
   double preco;
   int parcelas;
-  char descricao[4096];
+  char descricao[PROD_DESC_SIZE];
 } Produto;
 
+/**
+ * Inicializa o módulo produto.
+ *
+ * @param pool pool de conexões com o banco de dados.
+ */
 void produto_init(DBPool *pool);
 
+/**
+ * Cria uma nova instância de produto.
+ */
 Produto *produto_new(const char *nome);
 
 /**
  * Adiciona um produto na loja.
+ *
+ * @param produto produto a ser adicionado na loja.
+ * @return 0, em caso de sucesso, 1, em caso de erro.
  */
 int produto_add(const Produto *produto);
 
@@ -26,6 +41,9 @@ int produto_add(const Produto *produto);
  */
 int produto_buscar(Produto *produtos, int pg, int pgSize);
 
+/**
+ * Destroi uma instância de produto.
+ */ 
 void produto_free(Produto *produto);
 
 #endif
